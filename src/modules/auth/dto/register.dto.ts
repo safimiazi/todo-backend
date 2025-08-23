8314381d14ee92073ebc9230a1f8f0fbd18bbecd
unknown <Arman8957>
2025-08-23 12:35:24 +0600

    db updated

---
+++ src/modules/auth/dto/register.dto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsString, MinLength, IsOptional } from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  password: string;
+
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsOptional()
+  @IsString()
+  avatar?: string;
+}
