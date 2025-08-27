212d91d589da6aed5af3decfcae77255c348d31e
safimiazi <mohibullamiazi@gmail.com>
2025-08-27 12:04:31 +0600

    script updated

--- prisma/migrations/20250826090119_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."clips" ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT true;
-
--- AlterTable
-ALTER TABLE "public"."users" ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT true;
--- prisma/migrations/20250826104327_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."users" ADD COLUMN     "isDeleted" BOOLEAN NOT NULL DEFAULT true;
